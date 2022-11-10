61e53900886d85e73255107c50f2c995e0973eb3
Shion Ichikawa <shion1305@gmail.com>
2022-11-11 00:04:13 +0900

    ✨ Noxコマンドの挙動の変更(コマンド元チャンネルへ返信)

--- src/main/java/com/shion1305/lumos/discord/NoxCommand.java
+++ src/main/java/com/shion1305/lumos/discord/NoxCommand.java
@@ -16,7 +16,6 @@ import java.util.Random;
 import java.util.logging.Logger;
 
 public class NoxCommand {
-    private static Channel channel;
     private static final String[] msg = new String[]{"お疲れ!", "Goodbye! See you again!!", "See ya!", "またな!", "printf(\"Goodbye, world!\");", "続けるにはENTERを押すかコマンドを入力してください", "terminated with status code 0", "Console.WriteLine(\"Goodbye, world!\");"};
     private static final Random random = new Random();
     private static final Logger logger = Logger.getLogger("NoxCommand");
@@ -26,8 +25,6 @@ public class NoxCommand {
     }
 
     synchronized static void initiate() {
-        channel = DiscordClientManager.getClient().getChannelById(Snowflake.of(ConfigManager.getConfig(ConfigManager.Config.NOX_CHANNEL)))
-                .block();
         ApplicationCommandRequest request = ApplicationCommandRequest.builder()
                 .type(ApplicationCommand.Type.CHAT_INPUT.getValue())
                 .name("nox")
@@ -43,20 +40,28 @@ public class NoxCommand {
                 })
                 .subscribe(data ->
                         CommandManager.addCommand(data.name(), event -> {
-                            event.deferReply().withEphemeral(true).block();
-                            Optional<Member> o_member = event.getInteraction().getMember();
-                            if (o_member.isEmpty()) {
-                                logger.warning("NoxCommand failed to get member");
-                                return;
+                            try {
+                                event.deferReply().withEphemeral(true).block();
+                                Optional<Member> o_member = event.getInteraction().getMember();
+                                if (o_member.isEmpty()) {
+                                    logger.warning("NoxCommand failed to get member");
+                                    return;
+                                }
+                                Member member = o_member.get();
+                                Channel channel = event.getInteraction().getChannel().block();
+                                if (member.getVoiceState().block() != null) {
+                                    assert channel != null;
+                                    channel.getRestChannel().createMessage(createNoxMessage(member).asRequest()).block();
+                                    event.createFollowup("Noxコマンドを発動しました! お疲れ!").block();
+                                } else {
+                                    event.createFollowup("このコマンドはボイスチャンネルに入っている時のみ有効です。").block();
+                                }
+                            } catch (Exception e) {
+                                logger.severe("ERROR in Nox command...");
+                                e.printStackTrace();
                             }
-                            Member member = o_member.get();
-                            if (member.getVoiceState().block() != null) {
-                                channel.getRestChannel().createMessage(createNoxMessage(member).asRequest()).block();
-                                event.createFollowup("Noxコマンドを発動しました! お疲れ!").block();
-                            } else {
-                                event.createFollowup("このコマンドはボイスチャンネルに入っている時のみ有効です。").block();
-                            }
-                        }));
+                        })
+                );
     }
 
     public static EmbedCreateSpec createNoxMessage(Member member) {
